e342e0ac1b32b613a44dea86e5cbacab41fb63d1
atari-monk <atari.monk1@gmail.com>
2025-08-22 16:00:10 +0200

    Version 1.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { GameEngineFactory } from "./factories/game-engine-factory.js";
 export { GameEngine } from "./core/game-engine.js";
 export type { Scene } from "./interfaces/scene.js";
+
+export type { InputSystem } from "./systems/input/input-system.js";
